474239b71b6e711dfc766ddd102d0d48085b71d4
眞壁 宏尭 <s01717@CG3890.local>
2018-04-19 11:13:06 +0900

    行の削除機能と、達成の切り替えを追加

--- Todo/Todo.js
+++ Todo/Todo.js
@@ -3,10 +3,12 @@
 //生成されたものにはチェックとバツボタンがある
 
 function AddTodo(code) {
-    if (code == 13) {
-        let tex = document.getElementById("textField");
+    let tex = document.getElementById("textField");
+    if (code === 13 && tex.value !== " ") {
         CreateTodoItem(tex.value);
         tex.value = " ";
+
+        UpdateListCount();
     }
 }
 
@@ -22,15 +24,20 @@ function CreateTodoItem(todoText) {
     let row = table.insertRow(1);
     // セルの挿入
     var cell1 = row.insertCell(-1);
+    var cell2 = row.insertCell(-1);
+    var cell3 = row.insertCell(-1);
     // ボタン用 HTML
-    var button = '<input type = "text"  value = ' + todoText + '>';
+    var checkBox = '<input name="flag" type="checkbox" value="1" onchange="SwitchState(this)" />';
+    var textBox = '<p>' + todoText + '</p>';
+    var button = '<input type="button" value="×" onclick="DeleteRow(this)">';
+    //TODO:セルの内容
+
     // セルの内容入力
-    cell1.innerHTML = button;
+    cell1.innerHTML = checkBox;
+    cell2.innerHTML = textBox;
+    cell3.innerHTML = button;
 
 
-    var num = list.rows.length;
-    console.log(num);
-    document.getElementById("text").innerHTML = (num - 2) + " items left"
     //TODO:要素を組み合わせたものを生成するにはどうするか
     //完成品を作って置いてそれをコピーするとか
 
@@ -39,5 +46,42 @@ function CreateTodoItem(todoText) {
     sampleButton.textContent = todoText;
     cell1 = sampleButton;
     */
+}
 
+function DeleteRow(obj) {
+    console.log("けすど");
+
+    tr = obj.parentNode.parentNode;
+    tr.parentNode.deleteRow(tr.sectionRowIndex);
+
+    UpdateListCount();
+}
+
+function DeleteAllRow() {
+    const table = document.getElementById("list");
+
+    for (i = 1; i < table.rows.length; i++) {
+        console.log(i);
+        table.deleteRow(i);
+    }
+
+    UpdateListCount();
+}
+
+function SwitchState(obj) {
+    var tr = obj.parentNode.parentNode;
+    const text = list.rows[tr.sectionRowIndex].cells[1].firstChild;
+    console.log(tr.sectionRowIndex)
+    console.log(text);
+
+    if (text.style.textDecoration === "line-through") {
+        text.style.textDecoration = "none";
+    } else {
+        text.style.textDecoration = "line-through";
+    }
+}
+
+function UpdateListCount() {
+    const num = list.rows.length;
+    document.getElementById("text").innerHTML = (num - 2) + " items left"
 }
