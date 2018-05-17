308965c60ea031c1e452761182c7998b0b8e1b74
Nimame <hiroaki.makabe714@gmail.com>
2018-05-17 23:10:08 +0900

    CodeIgniterのチュートリアル完了

--- Exercise-PHP/php7codeigniter-on-docker/php/application/config/routes.php
+++ Exercise-PHP/php7codeigniter-on-docker/php/application/config/routes.php
@@ -49,5 +49,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:    my-controller/index    -> my_controller/index
 |        my-controller/my-method    -> my_controller/my_method
  */
-$route['default_controller'] = 'pages/view';
+$route['news/create'] = 'news/create';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
---
+++ Exercise-PHP/php7codeigniter-on-docker/test.sql
@@ -0,0 +1,4 @@
+insert into news(id, title, slug, text) 
+values(0,'soccer','soc','fine'),
+values(0,'baseball','bas','good'),
+values(,'shooting','sho','great')
